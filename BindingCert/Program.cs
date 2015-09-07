a5f40b2359453811744193ee28cde88c53441202
pcontat <contat.paul@gmail.com>
2015-09-07 17:36:52 -0400

    add https support

---
+++ BindingCert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BindCertToPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 1234;
+            string certPath = "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v7.1A\\Bin\\MyAdHocTestCert.cer";
+            X509Certificate2 certificate = new X509Certificate2(certPath);
+
+            // netsh http add sslcert ipport=0.0.0.0:<port> certhash={<thumbprint>} appid={<some GUID>}
+            Process bindPortToCertificate = new Process();
+            bindPortToCertificate.StartInfo.FileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.SystemX86), "netsh.exe");
+            bindPortToCertificate.StartInfo.Arguments = string.Format("http add sslcert ipport=0.0.0.0:{0} certhash={1} appid={{{2}}}", port, certificate.Thumbprint, Guid.NewGuid());
+            bindPortToCertificate.Start();
+            bindPortToCertificate.WaitForExit();
+        }
+    }
+}
--- SondageService/Server.cs
+++ SondageService/Server.cs
@@ -9,19 +9,19 @@ namespace USherbrooke.ServiceModel.Sondage
         static void Main(string[] args)
         {
             // Specify a base address for the service
-            Uri httpBaseAddress = new Uri("http://localhost:1234/SondageWS");
+            Uri httpBaseAddress = new Uri("https://localhost:1234/SondageWS");
             ServiceHost poolServiceHost = null;
             // Création du service
             using (poolServiceHost = new ServiceHost(typeof(SondageService), httpBaseAddress))
             {
                 BasicHttpBinding binding = new BasicHttpBinding();
-                //binding.Security.Mode = BasicHttpSecurityMode.Transport;
+                binding.Security.Mode = BasicHttpSecurityMode.Transport;
                 //Add Endpoint to Host
                 poolServiceHost.AddServiceEndpoint(typeof(ISondageService), binding, httpBaseAddress);
                     
                 //Metadata Exchange
                 ServiceMetadataBehavior serviceBehavior = new ServiceMetadataBehavior();
-                serviceBehavior.HttpGetEnabled = true;
+                serviceBehavior.HttpsGetEnabled = true;
                 serviceBehavior.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
                 poolServiceHost.Description.Behaviors.Add(serviceBehavior);
                     
